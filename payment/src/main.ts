b3f6ea015149c97b22ef4b3be9a021865d2505ad
Drioux Guidry <driouxguidry@nxuenergy.com>
2023-10-09 17:33:04 -0700

    Add buffer logs

--- payment/src/main.ts
+++ payment/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from "./app/app.module";
 import { Logger } from "nestjs-pino";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { bufferLogs: true });
 
   const options = new DocumentBuilder()
     .setTitle("Payment Microservice")
