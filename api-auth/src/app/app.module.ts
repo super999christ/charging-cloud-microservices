f6a98b5deaec677ce3b8fc5f9d4bccc99b916924
Drioux Guidry <driouxguidry@nxuenergy.com>
2023-10-09 17:28:34 -0700

    Add additional logging to all cloud microservices

--- api-auth/src/app/app.module.ts
+++ api-auth/src/app/app.module.ts
@@ -1,9 +1,15 @@
-import { Module } from '@nestjs/common';
-import { AuthenticationModule } from '../authentication/authentication.module';
-import { AppController } from './app.controller';
+import { Module, RequestMethod } from "@nestjs/common";
+import { AuthenticationModule } from "../authentication/authentication.module";
+import { AppController } from "./app.controller";
+import { LoggerModule } from "nestjs-pino";
 
 @Module({
   controllers: [AppController],
-  imports: [AuthenticationModule]
+  imports: [
+    AuthenticationModule,
+    LoggerModule.forRoot({
+      exclude: [{ method: RequestMethod.ALL, path: "healthz" }],
+    }),
+  ],
 })
 export class AppModule {}
--- payment/src/app/app.module.ts
+++ payment/src/app/app.module.ts
@@ -3,16 +3,24 @@ import { AppController } from "./app.controller";
 import { ConfigModule } from "@nestjs/config";
 import { AuthMiddleware } from "../middlewares/auth.middleware";
 import { PaymentModule } from "../services/payment/payment.module";
+import { LoggerModule } from "nestjs-pino";
 
 @Module({
-  imports: [ConfigModule.forRoot(), PaymentModule],
+  imports: [
+    ConfigModule.forRoot(),
+    PaymentModule,
+    LoggerModule.forRoot({
+      exclude: [{ method: RequestMethod.ALL, path: "healthz" }],
+    }),
+  ],
   controllers: [AppController],
   providers: [],
 })
 export class AppModule {
   configure(consumer: MiddlewareConsumer): void {
-    consumer.apply(AuthMiddleware)
-    .exclude({ path: "/healthz", method: RequestMethod.GET })
-    .forRoutes("*");
+    consumer
+      .apply(AuthMiddleware)
+      .exclude({ path: "/healthz", method: RequestMethod.GET })
+      .forRoutes("*");
   }
 }
